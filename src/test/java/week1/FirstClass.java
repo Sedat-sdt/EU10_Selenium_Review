f6f65c0df6bdbd6e02de97372c0b0fe25104ded9
Sedat Maden <madensedat05@gmail.com>
2023-01-21 21:00:30 +0300

    4. week ended

--- src/test/java/week1/FirstClass.java
+++ src/test/java/week1/FirstClass.java
@@ -1,2 +1,15 @@
-package week1;public class FirstClass {
+package week1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+    }
 }
